4c9d3160547600e493553a5872d68a79b39acab9
Omar Oueidat <oueidat@student.chalmers.se>
2018-12-05 19:56:06 +0100

    StupidCode Lol

--- Day3/Day03.hs
+++ Day3/Day03.hs
@@ -12,14 +12,20 @@ type Pos = (Int,Int)
 data Claim = Claim {claimid::Int, pos::Pos, width::Int, height::Int}
     deriving (Show,Eq)
 
-part1 :: FilePath -> IO [Claim]
-part1 file = readFile file >>= 
-             \i -> 
-             return $ (rights . map readClaims . lines) i
-                
+part1 :: FilePath -> IO Int
+part1 file = do i <- readFile file 
+                let allPoints = (getAllClaimPoints . rights . map readClaims . lines) i
+                let mapOfClaims = foldr (\x ->  M.insertWith (+) x 1) M.empty allPoints
+                let listOfClaims = M.toList mapOfClaims
+                return $ length $ filter ( (>1) .snd) listOfClaims                
+
+
 
 parseInt :: Parser Int
 parseInt = read <$> many1 digit
+
+getAllClaimPoints :: [Claim] -> [Pos]
+getAllClaimPoints =  concatMap claimPoints 
 
 pair :: Char -> Parser (Int,Int)
 pair c = (,) <$> (spaces *> parseInt) <* char c <*> parseInt
