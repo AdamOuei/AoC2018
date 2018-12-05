1f1ee6751f00365836fa9768bdee26eb325f460c
Omar Oueidat <oueidat@student.chalmers.se>
2018-12-05 14:23:57 +0100

    New way to do do blocks

--- Day3/Day03.hs
+++ Day3/Day03.hs
@@ -1,22 +1,20 @@
--- import Data.Set
 import Data.List
 import Data.List.Split
 import Data.Maybe
 import Data.Either
---import qualified Data.Set as Set
 import Text.Parsec.Char(spaces, digit, char)
 import Text.Parsec.String(Parser)
 import Text.Parsec(parse, many1,ParseError)
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
 
 
 data Claim = Claim {claimid, x , y, width, height:: Int}
     deriving (Show,Eq)
 
 part1 :: FilePath -> IO [Claim]
-part1 file = do i <- readFile file
-                let claims = (rights . map readClaims . lines) i
-                return claims
+part1 file = readFile file >>= \i -> 
+             return $ (rights . map readClaims . lines) i
+                
 
 parseInt :: Parser Int
 parseInt = read <$> many1 digit
@@ -25,15 +23,17 @@ pair :: Char -> Parser (Int,Int)
 pair c = (,) <$> (spaces *> parseInt) <* char c <*> parseInt
 
 parseClaim :: Parser Claim
-parseClaim = do
-            char '#'
-            id <- parseInt
-            spaces
-            char '@'
-            (x,y) <- pair ','
-            char ':'
-            (width,height) <- pair 'x'
-            pure $ Claim {claimid = id,x = x ,y = y ,width = width,height = height}
+parseClaim = 
+            char '#' >>
+            parseInt >>= \id ->
+            spaces >>
+            char '@' >>
+            pair ',' >>= \(x,y) ->
+            char ':' >>
+            pair 'x' >>= \(width,height) ->
+            pure Claim {claimid = id,x = x ,y = y ,width = width,height = height}
+
+           
 
 readClaims :: String -> Either ParseError Claim
 readClaims = parse parseClaim "stdin"
