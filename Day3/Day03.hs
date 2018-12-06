461c56f1ba1ba4ea823838e7eab54862db8491c0
Omar Oueidat <oueidat@student.chalmers.se>
2018-12-06 21:00:29 +0100

    Complete day3

--- Day3/Day03.hs
+++ Day3/Day03.hs
@@ -15,7 +15,7 @@ data Claim = Claim {claimid::Int, pos::Pos, width::Int, height::Int}
 part1 :: FilePath -> IO Int
 part1 file = do i <- readFile file 
                 let allPoints = (getAllClaimPoints . rights . map readClaims . lines) i
-                let mapOfClaims = foldr (\x ->  M.insertWith (+) x 1) M.empty allPoints
+                let mapOfClaims = foldr (\x -> M.insertWith (+) x 1) M.empty allPoints
                 let listOfClaims = M.toList mapOfClaims
                 return $ length $ filter ( (>1) .snd) listOfClaims                
 
@@ -51,3 +51,37 @@ parseClaim =
 
 readClaims :: String -> Either ParseError Claim
 readClaims = parse parseClaim "stdin"
+
+
+
+-- Part 2
+getAllClaimTriplets :: [Claim] -> [(Pos,Int)]
+getAllClaimTriplets =  concatMap claimTriplets
+
+
+claimTriplets :: Claim -> [(Pos,Int)]
+claimTriplets claim = [((x',y'),cid) | x' <- [x..x+w-1] , y' <- [y..y+h-1]]
+                where (x,y) = pos claim
+                      w = width claim
+                      h = height claim
+                      cid = claimid claim
+getAllClaimIds :: [Claim]-> [Int]
+getAllClaimIds =  map claimIds
+
+claimIds :: Claim -> Int
+claimIds = claimid 
+                
+                      
+part2 :: FilePath -> IO [Int]
+part2 file = do i <- readFile file 
+
+                let --input =  (rights . map readClaims . lines) i
+                    allClaims = (getAllClaimTriplets . rights . map readClaims . lines) i
+                    allClaimIds = (getAllClaimIds . rights . map readClaims . lines) i
+                    mapOfClaims = foldr (\ (x, y) -> M.insertWith (++) x [y] ) M.empty allClaims
+                    listOfClaims = M.toList mapOfClaims
+                    listOfMapIds = filter (\x -> length x >1) (map snd listOfClaims)
+                    anotherList = (sort .nub . concat) listOfMapIds
+                    lastList = filter (`notElem` anotherList) allClaimIds
+                    in
+                        return lastList
